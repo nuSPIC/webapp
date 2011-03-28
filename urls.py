c5727dc1345830c4b234679133d8818d32d4f385
Yury V. Zaytsev <yury@shurup.com>
2011-03-28 15:08:09 +0200

    Cosmetics and grammar fixes

--- urls.py
+++ urls.py
@@ -5,14 +5,14 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 
 
-# Activating django admin
+# Activate Django admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^admin/', include(admin.site.urls)),
 )
 
-# Serving static files in django development server mode
+# Serve static files in Django development server mode
 if settings.DEBUG:
     urlpatterns += patterns('',
         (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
