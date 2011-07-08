8a5caec47533683fa4a46d98fbf6b4b5e85ce9b9
Sebastian Spreizer <spreizer@web.de>
2011-07-08 12:15:10 +0200

    Network helpers

---
+++ network/helpers.py
@@ -0,0 +1,26 @@
+
+def values_extend(values, unique=False, toString=False):
+    """
+    Extend targets/sources e.g. if target is '1-3, 5', it converts into '1,2,3,5'.
+    """
+    
+    value_list = values.split(',')
+    extended_list = []
+    for value in value_list:
+	if '-' in value:
+	    start, end = value.split('-')
+	    value = [i for i in range(int(start), int(end)+1)]
+	    extended_list.extend(value)
+	else:
+	    extended_list.append(int(value))
+	    
+    # make each target value unique and sorted
+    if unique:
+	extended_list = list(set(extended_list))
+	extended_list.sort()
+	
+    # converts each value into string
+    if toString:
+	extended_list = map(lambda x: str(x), extended_list)
+	
+    return extended_list
