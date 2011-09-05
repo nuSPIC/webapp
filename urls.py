287bb8b128b3f34b5faeec448daa2f1ef6698bef
sysad <sysad@mayflower.(none)>
2011-09-05 17:19:36 +0200

    change urls

--- urls.py
+++ urls.py
@@ -1,8 +1,9 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
 from django.conf import settings
 from django.conf.urls.defaults import *
 from django.contrib import admin
+from djcelery import views as celery_views
 
 
 # Activate Django admin
@@ -15,11 +16,13 @@ urlpatterns = patterns('',
     (r'^feed/', include('feed.urls')),
     (r'^forum/', include('forum.urls')),
     (r'^network/', include('network.urls')),
-    (r'^result/', include('result.urls')),
+    (r'^result/',  include('result.urls')),
+
+    url(r'^(?P<task_id>[\w\d\-]+)/status/?$', celery_views.task_status, name="celery-task_status"),
 )
 
 # Serve static files in Django development server mode
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    )
+#if settings.DEBUG:
+urlpatterns += patterns('',
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+)
