10ed5caa4d6510e40752effc45f006a63583b412
sysad <sysad@mayflower.(none)>
2011-09-05 13:19:14 +0200

    Change comments.

--- network/helpers.py
+++ network/helpers.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 
 def values_extend(values, unique=False, toString=False):
     """
@@ -7,20 +8,20 @@ def values_extend(values, unique=False, toString=False):
     value_list = values.split(',')
     extended_list = []
     for value in value_list:
-	if '-' in value:
-	    start, end = value.split('-')
-	    value = [i for i in range(int(start), int(end)+1)]
-	    extended_list.extend(value)
-	else:
-	    extended_list.append(int(value))
-	    
-    # make each target value unique and sorted
+        if '-' in value:
+            start, end = value.split('-')
+            value = [i for i in range(int(start), int(end)+1)]
+            extended_list.extend(value)
+        else:
+            extended_list.append(int(value))
+            
+    # Make each target value unique and sorted
     if unique:
-	extended_list = list(set(extended_list))
-	extended_list.sort()
-	
-    # converts each value into string
+        extended_list = list(set(extended_list))
+        extended_list.sort()
+        
+    # Convert each value into string
     if toString:
-	extended_list = map(lambda x: str(x), extended_list)
-	
+        extended_list = map(lambda x: str(x), extended_list)
+        
     return extended_list
