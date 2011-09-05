5d8b830a56af4a97e2bf6154209ab687179cc235
sysad <sysad@mayflower.(none)>
2011-09-05 13:18:00 +0200

    Change comments in urls.

--- network/urls.py
+++ network/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('network.views',
     url(r'^$', 'network_list', name='network_list'),
     url(r'^(?P<SPIC_id>\d+)/(?P<local_id>\d+)/$', 'network', name='network'),
     
-    # These following requests are in Ajax.
+    # These following requests work with AJAX.
     url(r'^ajax/(?P<network_id>\d+)/(?P<version_id>\d+)/simulate/$', 'simulate', name='simulate'),
     url(r'^ajax/(?P<network_id>\d+)/device_preview/$', 'device_preview', name='device_preview'),
     url(r'^ajax/(?P<network_id>\d+)/device_commit/$', 'device_commit', name='device_commit'),
