aba3e8fc99e076ef357c24c51044f17c33566e06
sysad <sysad@mayflower.(none)>
2011-09-05 12:58:37 +0200

    Change urls

--- network/urls.py
+++ network/urls.py
@@ -1,18 +1,9 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls.defaults import patterns, url
-from django.views.generic import list_detail
-from network.models import Network
 
-network_info = {
-    "queryset" : Network.objects.filter(user_id=0),
-}
-
-urlpatterns = patterns ('', 
-    url(r'^$', list_detail.object_list, network_info, name='network_list'),
-    )
-
-urlpatterns += patterns('network.views',
-    
-    url(r'^(?P<SPIC_id>\d+)/(?P<local_network_id>\d+)/$', 'network', name='network'),
+urlpatterns = patterns('network.views',
+    url(r'^$', 'network_list', name='network_list'),
+    url(r'^(?P<SPIC_id>\d+)/(?P<local_id>\d+)/$', 'network', name='network'),
     
     # These following requests are in Ajax.
     url(r'^ajax/(?P<network_id>\d+)/(?P<version_id>\d+)/simulate/$', 'simulate', name='simulate'),
