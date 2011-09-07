0ea7569711de20adfaf58b4997b9669affbc0c42
babsey <sysad@mayflower.(none)>
2011-09-07 11:29:02 +0200

    Change result admin

--- result/admin.py
+++ result/admin.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 from models import Result
 
 class ResultAdmin(admin.ModelAdmin):
-    list_display = ('id', '__unicode__', 'revision', 'revision_date_created', 'has_spike_detector', 'has_voltmeter')
+    list_display = ('id', '__unicode__', 'revision', 'favorite', 'has_spike_detector', 'has_voltmeter')
+    list_filter = ['favorite', 'has_spike_detector', 'has_voltmeter']
 admin.site.register(Result, ResultAdmin)
