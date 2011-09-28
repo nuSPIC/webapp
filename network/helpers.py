d47e86c58207308df16de1a76bd37ce450986353
babsey <sysad@mayflower.(none)>
2011-09-28 18:32:09 +0200

    add function id_convert

--- network/helpers.py
+++ network/helpers.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import numpy as np
 
 def values_extend(values, unique=False, toString=False):
     """
@@ -15,13 +16,24 @@ def values_extend(values, unique=False, toString=False):
         else:
             extended_list.append(int(value))
             
-    # Make each target value unique and sorted
+    # Make each target/source value unique and sorted
     if unique:
         extended_list = list(set(extended_list))
-        extended_list.sort()
         
     # Convert each value into string
     if toString:
         extended_list = map(lambda x: str(x), extended_list)
         
     return extended_list
+
+
+def id_convert(ids, tid=None, vid=None):
+    tids = ids[:,0]
+    vids = np.array(ids[:,1], dtype=int)
+    
+    if tid in tids: 
+        return vids[tids.tolist().index(str(tid))]
+    if vid in vids: 
+        return tids[vids.tolist().index(int(vid))]
+        
+    return 
