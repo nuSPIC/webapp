6ca5db4dd76afdf60d95b3d3001bbfc508d53ed3
babsey <sysad@mayflower.(none)>
2011-10-07 14:35:52 +0200

    add network.js

---
+++ media/js/network.js
@@ -0,0 +1,74 @@
+$.fn.loadTheme = function (options) {
+        $( this ).addClass( "ui-widget ui-helper-clearfix ui-corner-top ui-shadow" )
+            .find( ".portlet-header" )
+                    .addClass( "ui-widget-header ui-corner-top tipTip-top" )
+                    .prepend( "<span class='ui-icon toggle ui-icon-minusthick'></span>")
+//                  .prepend( "<span class='ui-icon dialog ui-icon-newwin'></span>")
+                    .end()
+            .find( ".portlet-content" )
+                    .addClass( "ui-widget-content" );
+                    
+                    
+        $( this ).find( ".portlet-header .toggle" ).click(function() {
+                $( this ).toggleClass( "ui-icon-minusthick" ).toggleClass( "ui-icon-plusthick" );
+                $( this ).parents( ".portlet:first" ).find( ".portlet-content" ).slideToggle();
+        });
+        
+        $( this ).find( ".buttons")
+            .find( "button" ).button().end()
+            .find( ".delete" ).button({
+                    icons: {
+                        primary: "ui-icon-trash"
+                    },})
+                .end()
+            .find( ".check" ).button({
+                    icons: {
+                        primary: "ui-icon-check"
+                    },})
+                .end()
+            .find( ".reset" ).button({
+                    icons: {
+                        primary: "ui-icon-arrowreturnthick-1-w"
+                    },})
+                .end()
+            .find( ".play" ).button({
+                    icons: {
+                        primary: "ui-icon-play"
+                    },})
+                .end()
+            .find( ".zoomin" ).button({
+                    icons: {
+                        primary: "ui-icon-zoomin"
+                    }})
+                .end()
+            .find( ".help" ).button({
+                    icons: {
+                        primary: "ui-icon-help"
+                    }})
+                .end()        
+            .find( ".toggle" ).button({
+                    icons: {
+                        primary: "ui-icon-circle-arrow-s"
+                    }})
+                .end()
+            .find( ".ui-state-highlight" ).button({
+                    icons: {
+                        primary: "ui-icon-circle-arrow-n"
+                    }})
+                .end()
+            .find( ".document" ).button({
+                    icons: {
+                        primary: "ui-icon-document"
+                    }})
+                .end()
+            .find( ".favorite" ).button({
+                    icons: {
+                        primary: "ui-icon-star"
+                    }})
+                .end();
+                
+        $( this ).find( "div.field-wrapper" ).tipTip({
+                defaultPosition: 'right',
+                delay: 500,
+        });
+};
