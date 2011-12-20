73bbd4293ba0454bd1a5327378c656c3b0a5c352
babsey <sysad@mayflower.(none)>
2011-12-20 11:46:50 +0100

    small changes

--- network/urls.py
+++ network/urls.py
@@ -7,11 +7,11 @@ urlpatterns = patterns('network.views',
     url(r'^(?P<SPIC_id>\d+)/(?P<local_id>\d+)/layout/$', 'network_layout', name='network_layout'),
     url(r'^(?P<SPIC_id>\d+)/(?P<local_id>\d+)/(?P<result_id>\d+)/$', 'network_simulated', name='network_simulated'),
     
-    # These following requests work with AJAX.
+    # Following requests are responsing with AJAX.
     url(r'^ajax/(?P<network_id>\d+)/(?P<version_id>\d+)/simulate/$', 'simulate', name='simulate'),
     url(r'^ajax/(?P<network_id>\d+)/device_preview/$', 'device_preview', name='device_preview'),
     url(r'^ajax/(?P<network_id>\d+)/device_commit/$', 'device_commit', name='device_commit'),
     
-    url(r'^ajax/(?P<network_id>\d+)/save_layout/$', 'save_layout', name='save_layout'),
+    url(r'^ajax/(?P<network_id>\d+)/save_layout/$', 'layout_save', name='save_layout'),
     url(r'^ajax/(?P<network_id>\d+)/default_layout/$', 'default_layout', name='default_layout'),
     )
