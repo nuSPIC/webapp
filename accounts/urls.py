21951a8834362b147d265dc65f70c9c250ed888d
Sebastian Spreizer <spreizer@web.de>
2017-03-17 20:43:15 +0100

    fix bug to confirm user account

--- accounts/urls.py
+++ accounts/urls.py
@@ -35,7 +35,7 @@ urlpatterns += [
     url(r'^registration/$', views.registration, name='registration'),
     url(r'^registration/done/$', direct_to_template,
         {'template': 'registration/registration_done.html'}, name='registration_done'),
-    url(r'^registration/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', 'registration_confirm', name='registration_confirm'),
+    url(r'^registration/(?P<uidb36>[0-9A-Za-z]{1,13})-(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.registration_confirm, name='registration_confirm'),
     url(r'^registration/failed/$', direct_to_template,
         {'template': 'registration/registration_failed.html', 'extra_context': {'expire': settings.REGISTRATION_TIMEOUT_DAYS}}, name='registration_failed'),
     url(r'^registration/complete/$', direct_to_template,
